c5ca58485242c76991b0023f7b36b9b1d470938a
Hermann.M <marzeliereherman@gmail.com>
2024-03-19 14:27:01 +0100

    Redirection + import de l'autenthification du dernier projet

--- Connexion/Inscription/inscription.php
+++ Connexion/Inscription/inscription.php
@@ -1,7 +1,11 @@
 <?php
   session_start();
-  include '../functions.php';
 
+  //Redirection
+  if(isset($_SESSION['mail']) || isset($_SESSION['mail_p'])) {
+    header('Location: ../connexion.php');
+  }
+  
 ?>
 
 <!DOCTYPE html>
@@ -10,44 +14,47 @@
         <meta charset="utf-8">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-        <script src="accueil.js" defer></script>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-        <title> Accueil </title>
-        <link href="header.css" rel="stylesheet">
+        <title> Connexion </title>
+        <link href="../../header.css" rel="stylesheet">
+        <link href="inscription.css" rel="stylesheet">
     </head>
-
     <body>
-        
+
         <header>
+
             <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Accueil</a>
+                  <a class="navbar-brand" href="../../Accueil/accueil.php">Accueil</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
-
                   <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Mes RDV</a>
+                        <a class="nav-link active" aria-current="page" href="../../RDV/rdv.php">Mes RDV</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="Connexion/connexion.php">Connexion</a>
+
+                        <a class="nav-link" href="../connexion.php">Connexion</a>
+
                       </li>
                     </ul>
                   </div>
                 </div>
               </nav>
-        </header>
-        
-        <main>
 
+        </header>
+
+        <main>
+            <div id="inscription">
+                <form method="POST" align="center">
+                    <a class="type" href="InscriptionPatient/inscriptionpatient.php">Patient</a>
+                    <a class="type" href="InscriptionPracticien/inscriptionpracticien.php">Praticien</a>
+                </form>
+            </div>
         </main>
 
         <footer>
-
         </footer>
-
     </body>
-
 </html>
--- Connexion/connexion.php
+++ Connexion/connexion.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
-  include '../functions.php';
 
+  include '../functions.php';
 ?>
 
 <!DOCTYPE html>
@@ -10,44 +10,53 @@
         <meta charset="utf-8">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-        <script src="accueil.js" defer></script>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-        <title> Accueil </title>
-        <link href="header.css" rel="stylesheet">
+        <title> Connexion </title>
+        <link href="../header.css" rel="stylesheet">
+        <link href="connexion.css" rel="stylesheet">
     </head>
-
     <body>
-        
+
         <header>
+
             <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Accueil</a>
+                  <a class="navbar-brand" href="../index.php">Accueil</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
 
+                  <div class="ID">
+                  <?php
+                    affichage_utilisateur_connecte();
+                  ?>
+                  </div>
+                  
                   <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Mes RDV</a>
+                        <a class="nav-link active" aria-current="page" href="../index.php">Mes RDV</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="Connexion/connexion.php">Connexion</a>
+                        <a class="nav-link" href="connexion.php">Connexion</a>
                       </li>
                     </ul>
                   </div>
                 </div>
               </nav>
-        </header>
-        
-        <main>
 
+        </header>
+
+        <main>
+            <div id="connexion">
+                <form method="POST" align="center">
+                    <a href="SeConnecter/seconnecter.php">Se connecter</a>
+                    <a href="Inscription/inscription.php">S'inscrire</a>
+                    <a href="Deconnexion/deconnexion.php" id="deconnexion">Se déconnecter</a>
+                </form>
+            </div>
         </main>
 
         <footer>
-
         </footer>
-
     </body>
-
 </html>
