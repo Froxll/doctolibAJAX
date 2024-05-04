cea83f618017c610e7af676ebebfbfbbff853710
Nathan Gadbin <nathan.gadbin@isen-ouest.yncrea.fr>
2024-05-04 13:57:58 +0200

    recherche par nom + spécialité fonctionne, affichage rdv des différents médecins aussi

--- database.php
+++ database.php
@@ -1,4 +1,5 @@
 <?php
+
   require_once('constants.php');
 
   function dbConnect()
@@ -18,25 +19,68 @@
 
 
   
-  function dbRequestDoctor($db, $nom = '', $specialite = '')
-  {
+  function dbRequestDoctor($db, $nom = '', $specialite = ''){
     try {
-      $request = 'SELECT * FROM practicien';
-      if ($nom != '') {
-          $request .= ' WHERE nom LIKE :nom';
-          $nom .= '%';
-      }
+        $request = 'SELECT * FROM practicien';
+        $parameters = array();
+
+        if ($nom != '') {
+            $request .= ' WHERE nom LIKE :nom';
+            $nom .= '%';
+            $parameters[':nom'] = $nom;
+        }
+
+        if ($specialite != '') {
+            if ($nom != '') {
+                $request .= ' AND';
+            } else {
+                $request .= ' WHERE';
+            }
+            $request .= ' specialite LIKE :specialite';
+            $specialite .= '%';
+            $parameters[':specialite'] = $specialite;
+        }
+
+        $statement = $db->prepare($request);
+        $statement->execute($parameters);
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $exception) {
+        error_log('Request error: ' . $exception->getMessage());
+        return false;
+    }
+    return $result;
+}
+
+  function dbRequestRDV($db, $email){
+    try {
+      // Requête SQL pour récupérer les rendez-vous du praticien avec l'email spécifié
+      $request = 'SELECT * FROM RDV WHERE mail_1 = :email';
+
       $statement = $db->prepare($request);
-      if ($nom != '') {
-          $statement->bindParam(':nom', $nom, PDO::PARAM_STR, 20);
-      }
+      $statement->bindParam(':email', $email, PDO::PARAM_STR);
       $statement->execute();
       $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-  } catch (PDOException $exception) {
+    } catch (PDOException $exception) {
       error_log('Request error: ' . $exception->getMessage());
       return false;
-  }
+    }
   return $result;
+  }
+
+  function dbDeleteRDV($db, $id){   //Pas besoin de l'email du médecin car l'id rdv est unique
+    try
+    {
+      $request = 'DELETE FROM rdv WHERE id=:id';
+      $statement = $db->prepare($request);
+      $statement->bindParam(':id', $id, PDO::PARAM_INT);
+      $statement->execute();
+    }
+    catch (PDOException $exception)
+    {
+      error_log('Request error: '.$exception->getMessage());
+      return false;
+    }
+    return true;
   }
 
 ?>
